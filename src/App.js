ff1a83ec89e738dfe087636495434f0bb239c497
Jean Jean <jeanimodulo@outlook.com>
2024-10-31 18:54:43 -0300

    adds form submit and router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from "./components/Header";
 import FeedbackForm from "./components/FeedbackForm";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackData from "./data/FeedbackData";
 import FeedbackStats from './components/FeedbackStats';
+import AboutIconLink from './components/AboutIconLink';
+import AboutPage from './pages/AboutPage';
 
 export default function App() {
 
@@ -15,14 +19,32 @@ export default function App() {
         }
     }
 
+    const addFeedback = (newFeedback) => {
+
+        newFeedback.id = uuidv4();
+        setFeedback([newFeedback, ...feedback])
+
+    }
+
     return (
-        <>
+        <Router>
             <Header text="Feedback UI" />
             <div className="container">
-                <FeedbackForm />
-                <FeedbackStats feedback={feedback} />
-                <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+                <Routes>
+                    <Route exact path="/" element={
+                        <>
+                            <FeedbackForm handleAdd={addFeedback}/>
+                            <FeedbackStats feedback={feedback} />
+                            <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+                        </>
+                    }></Route>
+
+                    <Route path='/about' element={
+                        <AboutPage/>
+                    }></Route>
+                </Routes>
             </div>
-        </>
+            <AboutIconLink />
+        </Router>
     )
 }
